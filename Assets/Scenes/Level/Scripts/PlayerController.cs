5cb8a1d402de525012d56eda83c23ec889124b7f
Catoshepherd23 <william.mayes@snhu.edu>
2024-04-30 21:51:34 -0500

    Player can only jump once

--- Assets/Scenes/Level/Scripts/PlayerController.cs
+++ Assets/Scenes/Level/Scripts/PlayerController.cs
@@ -6,7 +6,10 @@ using TMPro;
 public class PlayerController : MonoBehaviour
 {
 
-    [SerializeField] private float speed;
+    [SerializeField] private float speed;  // Movement speed
+    [SerializeField] private float jumpForce;
+    private bool grounded;
+    
     private Rigidbody2D body;
     private int acornCount = 0;
 
@@ -21,6 +24,13 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
    private void Update()
    {
+        Movement();
+        Jump();
+        
+   }
+
+    private void Movement()
+    {
         float horizontalInput = Input.GetAxis("Horizontal");
         body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
 
@@ -31,13 +41,28 @@ public class PlayerController : MonoBehaviour
         // Checks if "a" is pressed to move left and flips the character facing left
         else if (horizontalInput < -0.01f)
             transform.localScale = new Vector3(-1, 1, 1);
+    }
 
-        // Checks to see if spacebar is pressed to initiate jump
-        if(Input.GetKey(KeyCode.Space))
-            body.velocity = new Vector2(body.velocity.x, speed);
-        
-   }
+    private void Jump()
+    {
+        // Checks to see if spacebar is pressed to initiate jump and is grounded
+        if(Input.GetKey(KeyCode.Space) && grounded)
+        {
+        body.velocity = new Vector2(body.velocity.x, jumpForce);
+        grounded = false;
+        }
+    }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Check if the player collides with an object tagged as "Ground"
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            grounded = true;
+        }
+    }
+
+    
     private void OnTriggerEnter2D(Collider2D collision)
     {
         // Checks if the player is interacting with the acorn via tag
