cec9d7eb9e8ed70120883f1d93821110c24926a4
Avry Luy <avryluy@mailbox.org>
2024-05-03 15:19:27 -0500

    Implement Sound toggle in main menu. reorganize audio script files.

--- Assets/Scenes/Main Menu/Scripts/Audio/MenuAudio.cs
+++ Assets/Scenes/Main Menu/Scripts/Audio/MenuAudio.cs
@@ -11,24 +11,30 @@ public class MenuAudio : MonoBehaviour
     [SerializeField] AudioClip[] UISelectAudioList;
     [SerializeField] AudioClip uiHoverAudio;
     int PrevIdx;
-    
-    int randomIdx()
+
+    int randomIdx(AudioClip[] audiocliparray)
     {
-        int idx = Random.Range(0, UISelectAudioList.Length);
+        int idx = Random.Range(0, audiocliparray.Length);
+        //Fix bug where audio index would be outside the range of the audioClipArray.
+        if (idx < audiocliparray.Length)
+            idx = Random.Range(0, audiocliparray.Length - 2);
+        if (idx > audiocliparray.Length)
+            idx = Random.Range(audiocliparray.Length - 2, audiocliparray.Length);
         return idx;
-    }    
+    }
+
     public void OnClick()
     {
         //Create clip index number to play random sound
-        int currentIdx = randomIdx();
-        AudioClip currentClip;
+        int currentIdx = randomIdx(UISelectAudioList);
+        AudioClip currentClip = UISelectAudioList[currentIdx];
         // if currentIdx eq PrevIdx, then re-randomize sound. Prevents 1 sound playing twice in a row.
-        if (currentIdx == PrevIdx)
-            currentClip = UISelectAudioList[currentIdx+1];
-        else
-            currentClip = UISelectAudioList[currentIdx];
-        Debug.Log("Current Index: " + currentIdx);
-        Debug.Log("Prev Index: " + PrevIdx);
+        //if (currentIdx == PrevIdx)
+        //    currentClip = UISelectAudioList[currentIdx+1];
+        //else
+        //    currentClip = UISelectAudioList[currentIdx];
+        //Debug.Log("Current Index: " + currentIdx);
+        //Debug.Log("Prev Index: " + PrevIdx);
 
         audioSource.PlayOneShot(currentClip);
         PrevIdx = currentIdx; 
